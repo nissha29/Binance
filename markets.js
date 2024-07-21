7f2f427c737f7dd0d2c3ce2ceae4aa0ee6f20dac
nissha29 <nishakashyap2907@gmail.com>
2024-07-21 20:03:13 +0530

    d/l mode

--- markets.js
+++ markets.js
@@ -29,3 +29,35 @@ async function getCryptoCoinDetails(){
 }
 
 getCryptoCoinDetails()
+
+
+const mode = document.querySelector('.mode')
+const nav = document.querySelector('.nav-bar')
+const login = document.querySelector('.login')
+
+function addLightMode(){
+    if(body.classList.contains('dark-mode')) body.classList.remove('dark-mode')
+    body.classList.add('light-mode')
+    body.style.backgroundColor = 'white'
+    body.style.color = '#181A20'
+    nav.style.backgroundColor = '#fff'
+    login.style.backgroundColor = '#c2c1c1'
+}
+
+function addDarkMode(){
+    if(body.classList.contains('light-mode')) body.classList.remove('light-mode')
+    body.classList.add('dark-mode')
+    body.style.backgroundColor = '#181A20'
+    body.style.color = '#c2c1c1'
+    nav.style.backgroundColor = '#181A20'
+    login.style.backgroundColor = '#2B3139'
+}
+
+mode.addEventListener('click', (e) => {
+    if(body.classList.contains('dark-mode')){
+        addLightMode()
+    }
+    else{
+        addDarkMode()
+    }
+})
